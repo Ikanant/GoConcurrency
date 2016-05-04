fe0ac5c41fd547c8602c4cfc5592fee7fd93d5d2
Ikanant <jonathan.hdez92@gmail.com>
2016-05-03 21:47:05 -0400

    removed First Example...  Created toggling Routines

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
+import (
+	"time"
+)
+
 func main() {
-	func() {
-		println("Hello")
+
+	godur, _ := time.ParseDuration("10ms")
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			println("Hello", i)
+			time.Sleep(godur)
+		}
 	}()
 
-	func() {
-		println("Go")
+	go func() {
+		for i := 0; i < 100; i++ {
+			println("Go", i)
+			time.Sleep(godur)
+		}
 	}()
+
+	// Let's put the main "routine" sleep for a bit
+	dur, _ := time.ParseDuration("1s")
+	time.Sleep(dur)
+
 }
