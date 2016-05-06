6b58684e80a328ad8ba9549eba7f297988f5f0ca
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-05-06 12:21:46 -0400

    Use channels to block a go routine from running until the previous one is done

--- LargerExample/main.go
+++ LargerExample/main.go
@@ -7,11 +7,14 @@ import (
 
 func main() {
 	runtime.GOMAXPROCS(4)
+	mutex := make(chan bool, 1)
 
 	for i := 1; i < 10; i++ {
 		for j := 1; j < 10; j++ {
+			mutex <- true
 			go func() {
 				fmt.Printf("%d + %d = %d\n", i, j, i+j)
+				<-mutex
 			}()
 		}
 	}
